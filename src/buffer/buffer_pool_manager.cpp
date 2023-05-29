f96689319a7a4798854a7b3b9ec43b2fedcb35d9
yuxintao <1921056015@qq.com>
2023-05-29 19:53:47 +0800

    完成project1的tack1

--- src/buffer/buffer_pool_manager.cpp
+++ src/buffer/buffer_pool_manager.cpp
@@ -11,9 +11,13 @@
 //===----------------------------------------------------------------------===//
 
 #include "buffer/buffer_pool_manager.h"
+#include <cstddef>
+#include <cstdlib>
 
+#include "common/config.h"
 #include "common/exception.h"
 #include "common/macros.h"
+#include "storage/page/page.h"
 #include "storage/page/page_guard.h"
 
 namespace bustub {
@@ -22,10 +26,6 @@ BufferPoolManager::BufferPoolManager(size_t pool_size, DiskManager *disk_manager
                                      LogManager *log_manager)
     : pool_size_(pool_size), disk_manager_(disk_manager), log_manager_(log_manager) {
   // TODO(students): remove this line after you have implemented the buffer pool manager
-  throw NotImplementedException(
-      "BufferPoolManager is not implemented yet. If you have finished implementing BPM, please remove the throw "
-      "exception line in `buffer_pool_manager.cpp`.");
-
   // we allocate a consecutive memory space for the buffer pool
   pages_ = new Page[pool_size_];
   replacer_ = std::make_unique<LRUKReplacer>(pool_size, replacer_k);
@@ -38,13 +38,71 @@ BufferPoolManager::BufferPoolManager(size_t pool_size, DiskManager *disk_manager
 
 BufferPoolManager::~BufferPoolManager() { delete[] pages_; }
 
-auto BufferPoolManager::NewPage(page_id_t *page_id) -> Page * { return nullptr; }
+auto BufferPoolManager::NewPage(page_id_t *page_id) -> Page * {
+  frame_id_t free_frame_id = -1;
+  if (!free_list_.empty()) {
+    free_frame_id = free_list_.front();
+    free_list_.pop_front();
+  } else {
+    if (!replacer_->Evict(&free_frame_id)) {
+      return nullptr;
+    }
+    if (pages_[free_frame_id].IsDirty()) {
+      disk_manager_->WritePage(pages_[free_frame_id].page_id_, pages_[free_frame_id].data_);
+    }
+    page_table_.erase(pages_[free_frame_id].page_id_);
+    pages_[free_frame_id].ResetMemory();
+  }
+  *page_id = AllocatePage();
+  pages_[free_frame_id].page_id_ = *page_id;
+  pages_[free_frame_id].pin_count_ = 1;
+  pages_[free_frame_id].is_dirty_ = false;
+  replacer_->RecordAccess(free_frame_id);
+  replacer_->SetEvictable(free_frame_id, false);  // no use
+  page_table_[*page_id] = free_frame_id;
+  return pages_ + free_frame_id;
+}
 
 auto BufferPoolManager::FetchPage(page_id_t page_id, [[maybe_unused]] AccessType access_type) -> Page * {
-  return nullptr;
+  if (page_table_.count(page_id) != 0) {
+    replacer_->RecordAccess(page_table_[page_id]);
+    replacer_->SetEvictable(page_table_[page_id], false);
+    pages_[page_table_[page_id]].pin_count_++;
+    return pages_ + page_table_[page_id];
+  }
+
+  frame_id_t free_frame_id = -1;
+  if (!free_list_.empty()) {
+    free_frame_id = free_list_.front();
+    free_list_.pop_front();
+  } else {
+    if (!replacer_->Evict(&free_frame_id)) {
+      return nullptr;
+    }
+    if (pages_[free_frame_id].IsDirty()) {
+      disk_manager_->WritePage(pages_[free_frame_id].page_id_, pages_[free_frame_id].data_);
+    }
+    page_table_.erase(pages_[free_frame_id].page_id_);
+    pages_[free_frame_id].ResetMemory();
+  }
+
+  pages_[free_frame_id].page_id_ = page_id;
+  pages_[free_frame_id].pin_count_ = 1;
+  pages_[free_frame_id].is_dirty_ = false;
+  replacer_->RecordAccess(free_frame_id);
+  replacer_->SetEvictable(free_frame_id, false);  // no use
+  page_table_[page_id] = free_frame_id;
+  return pages_ + free_frame_id;
 }
 
 auto BufferPoolManager::UnpinPage(page_id_t page_id, bool is_dirty, [[maybe_unused]] AccessType access_type) -> bool {
+  if (page_table_.count(page_id) > 0) {
+    frame_id_t frame_id = page_table_[page_id];
+    pages_[frame_id].is_dirty_ = is_dirty;
+    if (--pages_[frame_id].pin_count_ == 0) {
+      replacer_->SetEvictable(frame_id, true);
+    }
+  }
   return false;
 }
 
@@ -62,6 +120,6 @@ auto BufferPoolManager::FetchPageRead(page_id_t page_id) -> ReadPageGuard { retu
 
 auto BufferPoolManager::FetchPageWrite(page_id_t page_id) -> WritePageGuard { return {this, nullptr}; }
 
-auto BufferPoolManager::NewPageGuarded(page_id_t *page_id) -> BasicPageGuard { return {this, nullptr}; }
+auto BufferPoolManager::NewPageGuarded(page_id_t *page_id) -> BasicPageGuard { return {this, NewPage(page_id)}; }
 
 }  // namespace bustub
--- src/buffer/lru_k_replacer.cpp
+++ src/buffer/lru_k_replacer.cpp
@@ -11,20 +11,71 @@
 //===----------------------------------------------------------------------===//
 
 #include "buffer/lru_k_replacer.h"
+#include <cmath>
+#include <mutex>
 #include "common/exception.h"
+#include "common/macros.h"
+#include "googletest/googletest/src/gtest-internal-inl.h"
 
 namespace bustub {
 
 LRUKReplacer::LRUKReplacer(size_t num_frames, size_t k) : replacer_size_(num_frames), k_(k) {}
 
-auto LRUKReplacer::Evict(frame_id_t *frame_id) -> bool { return false; }
+auto LRUKReplacer::Evict(frame_id_t *frame_id) -> bool {
+  std::lock_guard<std::mutex> lock_guard(latch_);
+  *frame_id = -1;
+  for (auto &p : node_store_) {
+    if (p.second.is_evictable_) {
+      if (*frame_id == -1 || p.second < node_store_[*frame_id]) {
+        *frame_id = p.second.fid_;
+      }
+    }
+  }
+  if (*frame_id != -1) {
+    Remove(*frame_id);
+    return true;
+  }
+  return false;
+}
 
-void LRUKReplacer::RecordAccess(frame_id_t frame_id, [[maybe_unused]] AccessType access_type) {}
+void LRUKReplacer::RecordAccess(frame_id_t frame_id, [[maybe_unused]] AccessType access_type) {
+  std::lock_guard<std::mutex> lock_guard(latch_);
+  BUSTUB_ASSERT(frame_id < static_cast<int>(replacer_size_), "frame_id is larger than or equal to replacer_size_");
+  if (node_store_.count(frame_id) == 0) {
+    node_store_[frame_id].fid_ = frame_id;
+  }
+  auto &node = node_store_[frame_id];
+  node.history_.push_front(current_timestamp_++);
+  if (node.history_.size() < k_) {
+    node.k_ = INFINITY;
+    return;
+  }
+  while (node.history_.size() > k_) {
+    node.history_.pop_back();
+  }
+  node.k_ = node.history_.front() - node.history_.back();
+}
 
-void LRUKReplacer::SetEvictable(frame_id_t frame_id, bool set_evictable) {}
+void LRUKReplacer::SetEvictable(frame_id_t frame_id, bool set_evictable) {
+  std::lock_guard<std::mutex> lock_guard(latch_);
+  BUSTUB_ASSERT(node_store_.count(frame_id) > 0, "frame_id should be used");
+  if (!node_store_[frame_id].is_evictable_ && set_evictable) {
+    curr_size_--;
+  } else if (node_store_[frame_id].is_evictable_ && !set_evictable) {
+    curr_size_++;
+  }
+  node_store_[frame_id].is_evictable_ = set_evictable;
+}
 
-void LRUKReplacer::Remove(frame_id_t frame_id) {}
+void LRUKReplacer::Remove(frame_id_t frame_id) {
+  std::lock_guard<std::mutex> lock_guard(latch_);
+  if (!node_store_[frame_id].is_evictable_) {
+    throw Exception("Remove a non-evictable frame");
+  }
+  node_store_.erase(frame_id);
+  --curr_size_;
+}
 
-auto LRUKReplacer::Size() -> size_t { return 0; }
+auto LRUKReplacer::Size() -> size_t { return curr_size_; }
 
 }  // namespace bustub
